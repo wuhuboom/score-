b8a5344c2dc1d409de2d619e2864e44416c18593
eric <48643613+EricZhenPeng@users.noreply.github.com>
2024-01-31 23:23:33 +0800

    定时任务模块

---
+++ score-quartz/src/main/java/com/score/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.score.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.score.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author score
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
