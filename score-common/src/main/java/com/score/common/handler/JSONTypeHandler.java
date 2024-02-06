d28f6062015a94f7424731ed15bfcf6bab92e331
eric <48643613+EricZhenPeng@users.noreply.github.com>
2024-02-06 23:24:28 +0800

    通用工具调整

--- score-common/src/main/java/com/score/common/handler/JSONTypeHandler.java
+++ score-common/src/main/java/com/score/common/handler/JSONTypeHandler.java
@@ -1,2 +1,28 @@
-package com.score.common.handler;public class JSONTypeHandler {
+package com.score.common.handler;
+
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.extension.handlers.FastjsonTypeHandler;
+
+import java.util.List;
+
+public class JSONTypeHandler extends FastjsonTypeHandler {
+
+    private final Class<? extends Object> type;
+
+    public JSONTypeHandler(Class<?> type) {
+        super(type);
+        this.type = type;
+    }
+
+    @Override
+    protected List parse(String json) {
+        return JSON.parseArray(json, type);
+//        return JSON.parseObject(json, type);
+    }
+
+    @Override
+    protected String toJson(Object obj) {
+        return super.toJson(obj);
+    }
+
 }
